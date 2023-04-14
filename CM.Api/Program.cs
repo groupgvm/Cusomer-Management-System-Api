6c403b16e43e9532effbbace6726c289cca3e0ce
Udith_Gayan <udith.indrakantha@gmail.com>
2023-04-15 01:22:57 +0530

    added global error handler middleware.

--- CM.Api/Program.cs
+++ CM.Api/Program.cs
@@ -1,3 +1,5 @@
+using CM.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -15,6 +17,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseMiddleware<GlobalErrorHandler>();
 
 app.UseHttpsRedirection();
 
